0f7f230f8c486215fab2d2ad4a26c431bae6e751
gopheramit <amitwpu@gmail.com>
2023-03-13 18:49:33 +0530

    adding another step to pipeline

--- main.go
+++ main.go
@@ -5,21 +5,45 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"os/exec"
 )
 
 func run(proj string, out io.Writer) error {
 	if proj == "" {
 		return fmt.Errorf("project directory is required:%w", ErrValidation)
 	}
-	args := []string{"build", ".", "errors"}
-	cmd := exec.Command("go", args...)
-	cmd.Dir = proj
-	if err := cmd.Run(); err != nil {
-		return &stepErr{step: "go build", msg: "go build failed", cause: err}
+	// args := []string{"build", ".", "errors"}
+	// cmd := exec.Command("go", args...)
+	// cmd.Dir = proj
+	// if err := cmd.Run(); err != nil {
+	// 	return &stepErr{step: "go build", msg: "go build failed", cause: err}
+	// }
+	// _, err := fmt.Fprintln(out, "Go build:sucess")
+	pipeline := make([]step, 2)
+	pipeline[0] = newStep(
+		"go build",
+		"go",
+		"Go Build:SUCCESS",
+		proj,
+		[]string{"build", ".", "errors"},
+	)
+	pipeline[1] = newStep(
+		"go test",
+		"go",
+		"Go Test:SUCCESS",
+		proj,
+		[]string{"test", "-v"})
+
+	for _, s := range pipeline {
+		msg, err := s.execute()
+		if err != nil {
+			return err
+		}
+		_, err = fmt.Fprintln(out, msg)
+		if err != nil {
+			return err
+		}
 	}
-	_, err := fmt.Fprintln(out, "Go build:sucess")
-	return err
+	return nil
 
 }
 
